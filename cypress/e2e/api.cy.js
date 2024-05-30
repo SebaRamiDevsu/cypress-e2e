016efb03ff2fa5556e85c459c67daa2de5b6d2e9
SebaRamiDevsu <sebas.ramirez@devsu.com>
2024-05-30 13:07:03 -0500

    Add API test

---
+++ cypress/e2e/api.cy.js
@@ -0,0 +1,30 @@
+
+describe('API test for page demoblaze', () => {
+
+    it('Create new user in Sign Up', () => {
+        cy.request('POST','https://api.demoblaze.com/signup', { username:'sebasebasebasebasebas', password:'testtesttest'}).then(response => {
+            expect(response.body).eq('')
+        })
+    })
+
+    it('Create a user with the data of one already registered', () => {
+        cy.request('POST','https://api.demoblaze.com/signup', { username:'1', password:'1'}).then(response => {
+            expect(response.body).to.have.property('errorMessage', 'This user already exist.')
+        })
+    })
+
+    it('Login with a valid user', () => {
+        cy.request('POST','https://api.demoblaze.com/login', { username:'1', password:'MQ==' }).then(response => {
+            expect(response.body).to.contains('Auth_token') 
+            
+        })
+    })
+
+    it.only('Login with an invalid user', () => {
+        cy.request('POST','https://api.demoblaze.com/login', { username:'1', password:'1' }).then(response => {
+            expect(response.body).to.have.property('errorMessage', 'Wrong password.')
+            
+        })
+    })
+
+})
